75d4b4d491262abd9808e6d818c588a6586b3be5
fran sola <fmia1992@gmail.com>
2023-04-14 13:37:50 +0200

    start

---
+++ src/lib/page.svelte
@@ -0,0 +1,189 @@
+<div class="site-main">
+  <div class="container">
+    <div class="site__content">
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-10-pf-1.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-10-pf-2.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-10-pf-3.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-10-pf-4.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-10-pf-5.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-10-pf-6.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-10-pf-7.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-10-pf-8.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-1-pf-10.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-1-pf-9.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-1-pf-12.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-1-pf-11.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/09/m-1-pf-13.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/08/m-1-pf-1.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/08/m-1-pf-2.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/08/m-1-pf-3.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/08/m-1-pf-4.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/08/m-1-pf-5.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/08/m-1-pf-6.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/08/m-1-pf-7.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+      <figure>
+        <img
+          src="https://pisces.la-studioweb.com/wp-content/uploads/2017/08/m-1-pf-8.jpg"
+          alt="M 10 P 8"
+        />
+      </figure>
+    </div>
+  </div>
+</div>
+
+<style>
+  img {
+    /* width: 100%;
+    border-radius: 10px; */
+    border-radius: 1rem;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+  figure {
+    margin: 0;
+    transition: 0.3s;
+  }
+
+  /* figure:hover {
+    width: 300px !important;
+    transform: scale3d(1.2, 1.2, 1.2);
+  } */
+
+  .site-main {
+    margin-left: 160px;
+    padding: 2rem;
+  }
+
+  .container {
+    height: 100%;
+  }
+
+  .site__content {
+    display: block;
+    columns: 300px;
+    gap: 1rem;
+    /* display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(12, 150px);
+    margin: 0 auto;
+    max-width: 90%;
+    padding: 3rem 0; */
+  }
+
+  .site__content figure {
+    margin-bottom: 1rem;
+    break-inside: avoid;
+  }
+
+  /* .site__content figure:nth-child(odd) {
+    width: 3rem;
+    grid-row: 2/5;
+  }
+
+  .site__content figure:nth-child(even) {
+    width: 3rem;
+    grid-row: 1/4;
+  } */
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Page from "../lib/page.svelte";
+  import Sidebar from "../lib/sidebar.svelte";
+</script>
+
+<div class="app__inner">
+  <Sidebar />
+  <Page />
+</div>
+
+<style>
+  :global(body) {
+    background-color: whitesmoke;
+    font-family: "Poppins", sans-serif;
+    line-height: 24px;
+    font-weight: 400;
+  }
+
+  :global(*) {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  :global(.app) {
+    padding: 2rem;
+  }
+
+  .app__inner {
+    padding: 15px 30px;
+  }
+</style>
